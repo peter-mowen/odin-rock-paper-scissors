a2d923dd4351e537afb5781608f725d475026b67
Peter Mowen <peter.m.mowen@gmail.com>
2025-10-06 10:13:38 -0400

    Add getComputerChoice function
    Right now, the computer choice is just a random integer in the set
    {0, 1, 2} where {0 -> rock, 1 -> paper, 2 -> scissors}. In the future,
    we might update this function to actually use a strategy. For now, that
    strategy is random.
    
    Adds 10 console prints just to make sure we're getting an expected
    distribution. Will remove in a future commit.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,26 @@
+function getComputerChoice() {
+  computerChoice = Math.floor(Math.random()*3);
+
+  if (0 === computerChoice) {
+    return "rock";
+  } else if (1 === computerChoice) {
+    return "paper";
+  } else if (2 === computerChoice) {
+    return "scissors";
+  } else {
+    return null;
+  }
+}
+
 console.log(`Rock Paper Scissors`);
+
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
+console.log(`computerChoice = ${getComputerChoice()}`);
