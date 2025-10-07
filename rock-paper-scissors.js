f369163e760206ae343b226bcace107d42148eee
Peter Mowen <peter.m.mowen@gmail.com>
2025-10-07 14:57:49 -0400

    Add ability to play a round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,7 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  computerChoice = Math.floor(Math.random()*3);
+  let computerChoice = Math.floor(Math.random()*3);
 
   if (0 === computerChoice) {
     return "rock";
@@ -21,3 +21,43 @@ function getHumanChoice() {
   humanChoice = prompt(message);
   return humanChoice.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie!`);
+  } else if ("rock" === humanChoice) {
+    if ("paper" === computerChoice) {
+      console.log("You lose! paper covers rock!");
+      computerScore++;
+    }
+    else if ("scissors" === computerChoice) {
+      console.log("You win! rock smashes scissors!");
+      humanScore++;
+    }
+  } else if ("paper" === humanChoice) {
+    if ("scissors" === computerChoice) {
+      console.log("You lose! scissors cut paper!");
+      computerScore++;
+    } else if ("rock" === computerChoice) {
+      console.log("You win! paper covers rock!");
+      humanScore++;
+    }
+  } else if ("scissors" === humanChoice) {
+    if ("rock" === computerChoice) {
+      console.log("You lose! rocker smashes scissors");
+      computerScore++;
+    } else if ("paper" === computerChoice) {
+      console.log("You win! scissors cut paper!")
+      humanScore++;
+    }
+  }
+  return;
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+console.log("Rock...Paper...Scissors...Shoot!");
+console.log(`You threw ${humanChoice} and the computer threw ${computerChoice}`);
+playRound(humanChoice, computerChoice);
+console.log(`You: ${humanScore}, Computer: ${computerScore}`);
